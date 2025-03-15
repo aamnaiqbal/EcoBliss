5ec842ac0c721359285e73e1be542cec6a2ebea8
Aamna Iqbal <iqbal4502147@cloud.neduet.edu.pk>
2025-03-15 22:39:05 +0500

    App and main modified after shifting to redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.module.css";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import { PlantProvider } from "./store/PlantContext";
-import { PlantCareProvider } from "./store/plantCareContext";
-import CartProvider from "./store/CartContext";
 import ScrollToTop from "./components/ScrollToTop";
-import { AuthProvider } from "./store/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -15,28 +8,20 @@ function App() {
   return (
     <>
       <ScrollToTop />
-      <AuthProvider>
-        <PlantProvider>
-          <PlantCareProvider>
-            <CartProvider>
-              <ToastContainer
-                position="top-center"
-                autoClose={1000}
-                hideProgressBar={true}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="dark"
-                transition:Bounce
-              />
-              <Outlet />
-            </CartProvider>
-          </PlantCareProvider>
-        </PlantProvider>
-      </AuthProvider>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition:Bounce
+      />
+      <Outlet />
     </>
   );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import "./index.css";
 
-import router from "./router/Router.jsx"
-import { RouterProvider } from 'react-router-dom'
+import router from "./router/Router.jsx";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-
-    <RouterProvider router={router}/>
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
