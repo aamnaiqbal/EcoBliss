0d0d2558628e8595dd8a4cc7a0c176c19181e25c
Aamna Iqbal <iqbal4502147@cloud.neduet.edu.pk>
2025-03-21 23:06:05 +0500

    update Plant API added

--- server/Routes/vendorRoute.js
+++ server/Routes/vendorRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const vendorController = require("../Controllers/vendorCOntroller");
 const validateVendor = require("../Middlewares/validateVendorMiddleware");
+const upload = require("../Middlewares/multerMiddleware");
 const router = express.Router();
 
 router.route("/signup").post(validateVendor, vendorController.signUp);
@@ -9,6 +10,13 @@ router.route("/verify-otp").post(vendorController.verifyOTP);
 router.route("/resend-otp").post(vendorController.resendOTP);
 
 router.route("/plants/:vendorId").get(vendorController.getVendorPlants);
-router.route("/plants/:id/:vendorId").get(vendorController.getVendorPlant);
+router
+  .route("/plants/:id/:vendorId")
+  .get(vendorController.getVendorPlant)
+  .delete(vendorController.deletePlant);
+
+router
+  .route("/plants/:id")
+  .patch(upload.array("images", 4), vendorController.updatePlant);
 
 module.exports = router;
